4d4d9e454733633ac57fab9c394fe6ab4d6ff872
NiraStudio <33359000+NiraStudio@users.noreply.github.com>
2017-11-03 00:58:41 +0330

    testing the git

---
+++ PackManTest/Assets/Amir/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+        Vector2 t = transform.position;
+        t.x += hor * speed * Time.deltaTime;
+        t.y += ver * speed * Time.deltaTime;
+        transform.position = t;
+        
+	}
+}
